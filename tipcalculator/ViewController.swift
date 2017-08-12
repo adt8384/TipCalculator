c55884927840dfa8b1496ea97fac3596b11a15ea
Aditi Deshpande <adt8384@gmail.com>
2017-08-12 09:24:35 -0700

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Aditi Deshpande on 8/12/17.
+//  Copyright © 2017 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
