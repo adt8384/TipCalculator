34644972d9ef55df3588c432057680ba6b6d3790
Aditi Deshpande <adt8384@gmail.com>
2017-08-14 20:48:48 -0700

    Tipcalculator basic version

---
+++ tipcalculator/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  tipcalculator
+//
+//  Created by Aditi Deshpande on 8/13/17.
+//  Copyright © 2017 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func option(_ sender: Any) {
+    }
+    @IBAction func option1(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
